da930e3ce275047114352620a0bc6fba32019ca3
hashrock <hashedrock@gmail.com>
2016-08-25 22:49:14 +0900

    統合

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import Component from 'vue-class-component'
 import * as Vue from "vue";
-import {Junk, JunkUser, JunkItem, JunkItemBody} from "./junk"
+declare var firebase: any;
 
 @Component({
   props: {
@@ -23,10 +23,12 @@ import {Junk, JunkUser, JunkItem, JunkItemBody} from "./junk"
   `
 })
 export class App extends Vue {
-  junk: Junk;
-  user: JunkUser;
+  user: FirebaseUser;
   message: string;
-  items: Array<JunkItem>;
+  items: Array<FirebaseItem>;
+  private auth: any
+  private database: any
+  private ref: any
 
   data(): any{
     return {
@@ -37,28 +39,67 @@ export class App extends Vue {
   }
 
   ready(){
-    this.junk = new Junk((user: JunkUser)=>{
+    this.auth = firebase.auth();
+    this.database = firebase.database();
+    this.ref = this.database.ref('posts');
+    this.auth.onAuthStateChanged((user: FirebaseUser)=>{
       this.user = user;
-    }, (item: JunkItem)=>{
+    });
+    this.ref.off();
+    this.ref.limitToLast(30).on('child_added', (item: FirebaseItem)=>{
       console.log("fetched");
       this.items.push(item)
-    }, 30);
+    });
   }
   login(){
-    this.junk.login();
+    var provider = new firebase.auth.GoogleAuthProvider();
+    this.auth.signInWithPopup(provider);
   }
   logout(){
-    this.junk.logout();
+    this.auth.signOut();
   }
 
-  remove(post: JunkItem){
+  remove(post: FirebaseItem){
     this.items.$remove(post)
-    this.junk.remove(post.key);
+    this.ref.child(post.key).remove();
   }
 
   send(message: string){
-    this.junk.post(
-      message
-    )
+    var currentUser = this.auth.currentUser;
+    var item:FirebaseItemBody = {
+      author: {
+        uid: this.auth.currentUser.uid,
+        full_name: this.auth.currentUser.displayName,
+        screen_name: this.auth.currentUser.username ? this.auth.currentUser.username : "", //Only twitter
+        profile_picture: this.auth.currentUser.photoURL
+      },
+      text: message,
+      timestamp: firebase.database.ServerValue.TIMESTAMP
+    };
+    return this.ref.push(item)
   }
 }
+interface UploadCallback { (filePath: string): void }
+interface FetchCallback { (item: FirebaseItem): void }
+export class FirebaseItemBody{
+  author: {
+    uid: string,
+    full_name: string,
+    screen_name: string,
+    profile_picture: string
+  }
+  timestamp: number
+  text: string
+}
+interface FirebaseItemValue { (): FirebaseItemBody }
+export class FirebaseItem{
+  key: string
+  val: FirebaseItemValue
+}
+
+interface OnAuthStateChangedCallback {(user: FirebaseUser): void}
+export class FirebaseUser{
+  photoURL: string
+  displayName: string
+}
+
